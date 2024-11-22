6536af115114918d38acf2546eb8dc7824ec35c5
aldebarran22 <antonioespinherranz@hotmail.com>
2024-11-22 21:12:01 +0100

    21.11

---
+++ css/ejemplos/eventos/js/codigo.js
@@ -0,0 +1,45 @@
+addEventListener("load", () => {
+  addEventListener("click", (e) => {
+    console.log("click body");
+  });
+
+  let capa = document.getElementById("capa");
+  capa.addEventListener("click", (e) => {
+    console.log("click div");
+  });
+
+  let boton = document.getElementById("boton");
+  boton.addEventListener("click", (e) => {
+    console.log("click button");
+    // Cortar el evento:
+    e.stopPropagation();
+  });
+
+  let texto = document.getElementById("texto");
+  texto.addEventListener("keydown", (e) => {
+    console.log("e.keyCode:" + e.key);
+    if (e.key == "Enter") {
+      console.log("Pulsa enter");
+    }
+  });
+
+  let capa2 = document.getElementById("capa2");
+  capa2.addEventListener("mouseover", (e) => {
+    capa2.style.border = "2px solid red";
+  });
+  capa2.addEventListener("mouseout", (e) => {
+    capa2.style.border = "2px solid black";
+    texto.value = "";
+  });
+  capa2.addEventListener("mousemove", (e) => {
+    texto.value = e.clientX + ", " + e.clientY;
+  });
+
+  let parrafos = document.getElementsByTagName("p");
+  for (let p of parrafos) {
+    p.addEventListener("click", (e) => {
+      e.target.style.color = "red";
+      console.log(e.target.textContent);
+    });
+  }
+});
